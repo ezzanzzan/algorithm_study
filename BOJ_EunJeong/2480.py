b5b0c0a17c38fba05ff81bb207b2a9a958b6ce31
Eun Jeong <50096374+ezzanzzan@users.noreply.github.com>
2022-04-28 15:43:58 +0900

    [BOJ] 2480번 주사위 세개 / Bronze4

---
+++ BOJ_EunJeong/2480.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+dice = list(map(int,input().split()))
+
+if dice[0] == dice[1] and dice[1] == dice[2]:
+    print(10000+dice[1]*1000)
+elif dice[0] == dice[1] or dice[0] == dice[2]:
+    print(1000+dice[0]*100)
+elif dice[1] == dice[2]:
+    print(1000+dice[2]*100)
+else:
+    print(max(dice)*100)
